dfe4309941dee6313126b3897cc1d8ca80f9165b
hra06 <h.agarwal06@outlook.com>
2020-07-01 01:28:56 +0530

    Priority Queue

---
+++ Priority Queues/PriorityQueue.h
@@ -0,0 +1,27 @@
+#include<vector>
+using namespace std;
+
+
+class PriorityQueue{
+	vector<int> pq;
+	
+	public:
+		PriorityQueue(){
+		
+		}
+		
+		bool isEmpty(){
+			return (pq.size() == 0);
+		}
+		
+		int getSize(){
+			return pq.size();
+		}
+		
+		int getMin(){
+			if(isEmpty()){
+				return 0;
+			}
+			return pq[0];
+		}
+};
