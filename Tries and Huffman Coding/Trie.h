8c173200c51fbcfa45e24b8645e4ec4cc3dbfce3
hra06 <h.agarwal06@outlook.com>
2020-07-01 22:13:34 +0530

    Trie Class Done

--- Tries and Huffman Coding/Trie.h
+++ Tries and Huffman Coding/Trie.h
@@ -8,52 +8,92 @@ class Trie{
         Trie(){
             root = new TrieNode('\0');
         }
-        
-        void insertWord(TrieNode* root, string word){
-            //Base Case
-            if(word.size() ==0){
-                root->isTerminal = true;
-                return;
-            }
-            
-            //Small Calculation
-            int index = word[0]-'a';
-            TrieNode *child;
-            if(root->children[index] != NULL){
-                child = root->children[index];
-            }
-            else{
-                child = new TrieNode(word[0]);
-                root->children[index] = child;
-            }
-            
-            //Recursive Call
-            insertWord(child,word.substr(1));
-        }
-        
-        void insertWord(string word){
-            insertWord(root,word);
-        }
-        
-        bool search(TrieNode* root, string word){
-            if(word.size()==0){
-                if(root->isTerminal == true){
-                    return true;
+
+        private:    
+            void insertWord(TrieNode* root, string word){
+                //Base Case
+                if(word.size() ==0){
+                    root->isTerminal = true;
+                    return;
                 }
-                return false;
+                
+                //Small Calculation
+                int index = word[0]-'a';
+                TrieNode *child;
+                if(root->children[index] != NULL){
+                    child = root->children[index];
+                }
+                else{
+                    child = new TrieNode(word[0]);
+                    root->children[index] = child;
+                }
+                
+                //Recursive Call
+                insertWord(child,word.substr(1));
+            }
+
+        public:            
+            void insertWord(string word){
+                insertWord(root,word);
             }
             
-            //Small Calucaltion
-            int index = word[0]-'a';
-            if(root->children[index] !=NULL){
-                return search(root->children[index],word.substr(1));
+        private:
+            bool search(TrieNode* root, string word){
+                if(word.size()==0){
+                    if(root->isTerminal == true){
+                        return true;
+                    }
+                    return false;
+                }
+                
+                //Small Calucaltion
+                int index = word[0]-'a';
+                if(root->children[index] !=NULL){
+                    return search(root->children[index],word.substr(1));
+                }
+                else{
+                    return false;
+                }
             }
-            else{
-                return false;
-            }
-        }
         
-        bool search(string word){
-            return search(root,word);
-        }
+        public:
+            bool search(string word){
+                return search(root,word);
+            }
+
+
+        private:
+            void removeWord(TrieNode* root, string word){
+                if(word.size()==0){
+                    root->isTerminal = false;
+                    return;
+                }
+                
+                
+                TrieNode * child;
+                int index = word[0]-'a';
+                if(root->children[index] != NULL){
+                    child = root->children[index];
+                }else{
+                    return ;
+                }
+                
+                removeWord(child, word.substr(1));
+                
+                //Remove Child Node if it is useless
+                if(child->isTerminal ==false){
+                    for(int i =0 ;i<26;i++){
+                        if(child->children[i] !=NULL){
+                            return;
+                        }
+                    }
+                    delete child;
+                    root->children[index] =NULL;
+                }
+            }
+            
+        public:
+            void removeWord(string word){
+                return removeWord(root, word);
+            }
 };
