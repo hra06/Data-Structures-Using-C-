332fce23d085c281be4ebd18328f234ce4966570
hra06 <h.agarwal06@outlook.com>
2020-07-01 11:46:13 +0530

    Remove Min added

--- Priority Queues/PriorityQueue.h
+++ Priority Queues/PriorityQueue.h
@@ -45,4 +45,46 @@ class PriorityQueue{
 			}
 			
 		}
+		
+		
+		int removeMin() {
+            int min = pq[0];
+            int lastIndex = pq.size()-1;
+            int last = pq[lastIndex];
+            pq[0] = last;
+            pq.pop_back();
+            lastIndex--;
+            int parentIndex = 0;
+            while(2*parentIndex+1 <= lastIndex-1){
+                if(lastIndex > (2*parentIndex+1)){
+                    if(pq[parentIndex]<= pq[2*parentIndex+1] && pq[parentIndex]<= pq[2*parentIndex+2]){
+                        parentIndex++;
+                        break;
+                    }else if(lastIndex == 2*parentIndex+1 ){
+                        if(pq[lastIndex]<pq[parentIndex]){
+                            int temp = pq[parentIndex];
+                            pq[parentIndex] = pq[lastIndex];
+                            pq[lastIndex] = temp;
+                            break;
+                        }
+                    }else{
+                        int minIndex = 0;
+                        
+                        if(pq[parentIndex] <= pq[2*parentIndex+1] && pq[parentIndex] <= pq[2*parentIndex+2])
+                            minIndex = parentIndex;
+                        if(pq[2*parentIndex+1] <= pq[parentIndex]  && pq[2*parentIndex+1] <= pq[2*parentIndex+2])
+                            minIndex = 2*parentIndex+1;
+                        else
+                            minIndex = 2*parentIndex+2;
+                        
+                        int temp = pq[parentIndex];
+                        pq[parentIndex] = pq[minIndex];
+                        pq[minIndex] = temp;
+                        parentIndex = minIndex;
+                        
+                    }
+                }
+            }
+            return min;
+        }
 };
